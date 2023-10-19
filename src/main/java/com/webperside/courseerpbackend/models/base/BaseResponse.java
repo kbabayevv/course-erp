69bf244f4ef0f82aac93f6ab83e5eed5f269ae36
kbabayevv <kamran.babayevv@gmail.com>
2023-10-20 03:04:38 +0400

    first commit

---
+++ src/main/java/com/webperside/courseerpbackend/models/base/BaseResponse.java
@@ -0,0 +1,25 @@
+package com.webperside.courseerpbackend.models.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseResponse<T> {
+    HttpStatus httpStatus;
+    String message;
+    T data;
+
+    public static <T> BaseResponse<T> success(T data) {
+        return BaseResponse.<T>builder()
+                .httpStatus(HttpStatus.OK)
+                .message("Success")
+                .data(data)
+                .build();
+    }
+}
